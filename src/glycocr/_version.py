c1fe4365b4fd1d55f05981b88ce042d8e95e7b0f
Tom Stanton <tomdstanton@gmail.com>
2026-07-31 13:27:10 +1000

    📦 Cleaned pyproject.toml

--- src/glycocr/_version.py
+++ src/glycocr/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev2+g0481f923e.d20260730'
-__version_tuple__ = version_tuple = (0, 1, 'dev2', 'g0481f923e.d20260730')
+__version__ = version = '0.1.dev3+g0d03d6eef.d20260731'
+__version_tuple__ = version_tuple = (0, 1, 'dev3', 'g0d03d6eef.d20260731')
 
 __commit_id__ = commit_id = None
